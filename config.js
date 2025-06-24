ab091f85109ebfb8c3532faa44f1e79f4f134743
PhuHandSome <phuhandsome@localhost.localdomain>
2025-06-24 22:03:08 +0700

    cao quoc phu

--- config.js
+++ config.js
@@ -3,3 +3,10 @@ console.log('version11');
 console.log('test git config de chinh lai cai user name');
 // cao quoc phu da viet cai nay 
 console.log('ahihi')
+
+
+ahihi = () => {
+    console.log('fuck')
+}
+
+console.log(ahihi)
